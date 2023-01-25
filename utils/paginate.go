48c1491400ffd9993236b87f686a135582e622a7
kibetbrian <briankibet2040@gmail.com>
2023-01-25 13:02:16 +0300

    add pagination util

---
+++ utils/paginate.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"github.com/gofiber/fiber"
+	"shoppy/models"
+	"gorm.io/gorm"
+)
+
+func Paginate(db *gorm.DB, entity models.Entity, page int) fiber.Map {
+	limit := 15
+	offset := (page - 1) * limit
+
+	data := entity.Take(db, limit, offset)
+	total := entity.Count(db)
+
+	return fiber.Map{
+		"data": data,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      page,
+			"last_page": float64(int(total) / limit),
+		},
+	}
+}
