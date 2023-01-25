c0eb11589b5e44fcb3d78d8e3645f4b6e6207df2
kibetbrian <briankibet2040@gmail.com>
2023-01-25 11:33:41 +0300

    add login handler

--- handlers/auth.go
+++ handlers/auth.go
@@ -65,3 +65,51 @@ func Register(c fiber.Ctx) error {
 		"user":    user,
 	})
 }
+
+func Login(c *fiber.Ctx) error {
+	var req map[string]string
+
+	if err := c.BodyParser(&req); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	var count int64
+	database.Database().Model(&models.User{}).Where("email = ?", req["email"]).Count(&count)
+
+	if count == 0 {
+		c.JSON(404)
+		return c.JSON(fiber.Map{"success": false, "message": "user not found"})
+	}
+
+	if err := user.ComparePassword(req["password"]); err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"success": false,
+			"message": "email and password do not match",
+		})
+	}
+
+	token, err := utils.GenerateJwt(user.Id.String())
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	cookieExpiry := viper.Get("COOKIE_DURATION").(int)
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Duration(time.Duration(cookieExpiry).Hours())),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "login successfully",
+		"token":   token,
+		"user":    user,
+	})
+}
