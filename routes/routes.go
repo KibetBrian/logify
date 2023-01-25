b29939f5a6f381b436b329f3b0d67bc9654aadb1
kibetbrian <briankibet2040@gmail.com>
2023-01-25 12:58:28 +0300

    add routes

--- routes/routes.go
+++ routes/routes.go
@@ -5,6 +5,17 @@ import (
 	"shoppy/handlers"
 )
 
+const rootUrl = "/api/v1/"
+
 func Routes(app *fiber.App) {
 	app.Get("/", handlers.Hello)
+
+	//Auth
+	app.Post(rootUrl+"auth/register", handlers.Register)
+	app.Post(rootUrl+"auth/login", handlers.Login)
+	app.Get(rootUrl+"auth/logout", handlers.Logout)
+
+	//user
+	app.Post(rootUrl+"user/update", handlers.UpdateInfo)
+
 }
