5788e7c28858d97be547e03505056fe4c9329595
kibetbrian <briankibet2040@gmail.com>
2023-01-25 11:27:51 +0300

    add user registration handler

---
+++ handlers/auth.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"shoppy/database"
+	"shoppy/models"
+	"shoppy/utils"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	uuid "github.com/satori/go.uuid"
+	"github.com/spf13/viper"
+)
+
+func Register(c fiber.Ctx) error {
+	var req map[string]string
+
+	if err := c.BodyParser(&req); err != nil {
+		return err
+	}
+
+	id := uuid.NewV4()
+
+	user := models.User{
+		FirstName: req["f_name"],
+		LastName:  req["l_name"],
+		Email:     req["email"],
+		Phone:     req["phone"],
+		Id:        id,
+	}
+
+	user.SetPassword(req["password"])
+
+	var count int64
+	database.Database().Model(&models.User{}).Where("email = ?", req["email"]).Count(&count)
+
+	if count != 0 {
+		c.JSON(409)
+		return c.JSON(fiber.Map{"success": false, "message": "email already exists"})
+	}
+
+	database.Database().Create(&user)
+
+	uuidString := user.Id.String()
+
+	token, err := utils.GenerateJwt(uuidString)
+	if err != nil {
+		c.JSON(500)
+		return c.JSON(fiber.Map{"success": false, "message": "Internal server error"})
+	}
+
+	cookieExpiry := viper.Get("COOKIE_DURATION").(int)
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Duration(time.Duration(cookieExpiry).Hours())),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "Registered successfully",
+		"token":   token,
+		"user":    user,
+	})
+}
