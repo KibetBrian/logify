8076ce7566cdfc54856c4edda588e4e878fbfb5c
kibetbrian <briankibet2040@gmail.com>
2023-01-25 11:36:43 +0300

    add logout handler

--- handlers/auth.go
+++ handlers/auth.go
@@ -113,3 +113,18 @@ func Login(c *fiber.Ctx) error {
 		"user":    user,
 	})
 }
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "logout success",
+	})
+}
